c57be4c13120334fb85ed49af0017577981fcd0a
Vuthisak <sak@mango-byte.com>
2018-01-22 15:35:50 +0700

    change name Method to getCellphoneNumberWithNationalCode

--- domain/src/main/kotlin/com/ttvnp/ttj_asset_android_client/domain/model/PhoneNumber.kt
+++ domain/src/main/kotlin/com/ttvnp/ttj_asset_android_client/domain/model/PhoneNumber.kt
@@ -1,7 +1,7 @@
 package com.ttvnp.ttj_asset_android_client.domain.model
 
 class PhoneNumber(val nationalCode: String, val cellphoneNumber: String) {
-    fun getPhoneNumberWithNationalCode(): String {
+    fun getCellphoneNumberWithNationalCode(): String {
         if (nationalCode.isNotBlank() && cellphoneNumber.isNotBlank()) {
             return String.format("+%s %s", nationalCode, cellphoneNumber)
         }
